4c05dbec6bf336ec5b9d6cf5f30e2f7ffed81de4
Bin Gao <gaobin1019@gmail.com>
2018-01-07 15:54:36 -0800

    added getGeolocation when Home did mount

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Tabs, Button } from 'antd';
-const TabPane = Tabs.TabPane;
+import { GEO_OPTIONS } from "../constants"
 
+const TabPane = Tabs.TabPane;
 const operations = <Button>New post</Button>;
 
 export class Home extends React.Component {
+    //life cycle method should not be class property
+    //this location will not be changed through out the application for now
+    componentDidMount() {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS
+            );
+        } else {
+            //geolocation is not available
+            console.error('geolocation not supported')
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position);
+    }
+
+    onFailedLoadGeoLocation = (err) => {
+        console.log(err);
+    }
+
     render() {
         return (
             <Tabs className="main-tabs" tabBarExtraContent={operations}>
