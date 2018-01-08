0d241564d10ef0c9469c17fc9ad5b6eaf08a84eb
Bin Gao <gaobin1019@gmail.com>
2018-01-08 15:03:38 -0800

    added a spin while loading geolocation by using a state in Home componenet

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Tabs, Button } from 'antd';
+import { Tabs, Button, Spin } from 'antd';
 import { GEO_OPTIONS, POS_KEY } from "../constants"
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>New post</Button>;
 
 export class Home extends React.Component {
+    state = {
+        isLoadingGeoLocation: false,
+    };
+
     //life cycle method should not be class property
     //this location will not be changed through out the application for now
     componentDidMount() {
         if ("geolocation" in navigator) {
+            this.setState({isLoadingGeoLocation: true});
             navigator.geolocation.getCurrentPosition(
                 this.onSuccessLoadGeoLocation,
                 this.onFailedLoadGeoLocation,
@@ -23,6 +28,7 @@ export class Home extends React.Component {
 
     onSuccessLoadGeoLocation = (position) => {
         console.log(position);
+        this.setState({isLoadingGeoLocation: false});
 
         //destructor ES6
         const {latitude: lat, longitude: lon} = position.coords;
@@ -31,13 +37,23 @@ export class Home extends React.Component {
 
     onFailedLoadGeoLocation = (err) => {
         console.log(err);
+        this.setState({isLoadingGeoLocation: false});
     }
 
+    getGallery = () => {
+        if (this.state.isLoadingGeoLocation) {
+            return (
+                <Spin tip="Loading geolocation"></Spin>
+            );
+        }
+
+        return null;
+    }
     render() {
         return (
             <Tabs className="main-tabs" tabBarExtraContent={operations}>
                 <TabPane tab="Post" key="1">
-                    Content of tab 1
+                    {this.getGallery()}
                 </TabPane>
                 <TabPane tab="Map" key="2">
                     Content of tab 2
