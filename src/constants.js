8149cea493dd2bba612a7255b6c5762d686bc103
Bin Gao <gaobin1019@gmail.com>
2018-01-14 20:57:26 -0800

    .

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'https://around-75015.appspot.com';
+export const API_ROOT = 'https://aroundreact-190120.appspot.com';
 export const TOKEN_KEY = 'TOKEN_KEY';
 export const POS_KEY = 'POS_KEY';
 export const GEO_OPTIONS = {
